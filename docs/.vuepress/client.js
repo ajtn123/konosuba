fc7862f72f564e852dfa13b868d9f9ddbd5000ac
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-05-09 19:58:55 +0800

    compact section outline

--- docs/.vuepress/client.js
+++ docs/.vuepress/client.js
@@ -1,5 +1,6 @@
 import { defineClientConfig } from 'vuepress/client';
 import ImageView from './components/ImageView.vue';
+import './styles/index.css';
 
 export default defineClientConfig({
     enhance({ app }) {
